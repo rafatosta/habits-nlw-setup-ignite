00c68de3c512af66c1c8e60f51029e4ae47bf520
Rafael Tosta <rafa.ecomp@gmail.com>
2023-01-17 11:08:49 -0300

    Update Habit.tsx

--- web/src/components/Habit.tsx
+++ web/src/components/Habit.tsx
@@ -3,7 +3,14 @@ interface PropHabit {
 }
 
 function Habit(props: PropHabit) {
-  return <p>Habit: {props.completed}</p>;
+  return (
+    <div
+      className="flex items-center justify-center rounded-lg m-2
+     bg-zinc-600 text-white w-10 h-10 "
+    >
+      {props.completed}
+    </div>
+  );
 }
 
 export default Habit;
