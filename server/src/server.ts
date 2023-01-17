5eda19164e1caeafac6d1369d1987e4d9af476c1
Rafael Tosta <rafa.ecomp@gmail.com>
2023-01-17 10:36:04 -0300

    configuração do CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,14 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
 import { PrismaClient } from "@prisma/client";
 
 const app = Fastify();
 const prisma = new PrismaClient();
 
+app.register(cors);
+
 app.get("/", async () => {
-  const habits = await prisma.habit.findMany()
+  const habits = await prisma.habit.findMany();
   return habits;
 });
 
