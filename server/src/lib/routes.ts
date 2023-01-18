89d7bb72615de07eaaea83490d4c993571df1df2
Rafael Tosta <rafa.ecomp@gmail.com>
2023-01-18 11:52:21 -0300

    rota de criação de novo hábito

--- server/src/lib/routes.ts
+++ server/src/lib/routes.ts
@@ -1,5 +1,7 @@
 import { prisma } from "./prisma";
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import dayjs from "dayjs";
 
 export async function appRoutes(app: FastifyInstance) {
   app.get("/", async () => {
@@ -7,5 +9,28 @@ export async function appRoutes(app: FastifyInstance) {
     return habits;
   });
 
-  
+  app.post("/habits", async (req) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
+
+    const { title, weekDays } = createHabitBody.parse(req.body);
+
+    const today = dayjs().startOf("day").toDate();
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: today,
+        weekDays: {
+          create: weekDays.map((weekDay) => {
+            return {
+              week_day: weekDay,
+            };
+          }),
+        },
+      },
+    });
+  });
 }
