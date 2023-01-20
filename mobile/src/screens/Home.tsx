cdf589a5acd80b31161092b1c9b5265912a29bd3
Rafael Tosta <rafa.ecomp@gmail.com>
2023-01-20 14:57:03 -0300

    :iphone: componente HabitDay

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,10 +1,29 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
+import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
+
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 
 export function Home() {
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
+
+      <View className="flex-row mt-6 mb-2 justify-center ">
+        {weekDays.map((day, i) => {
+          return (
+            <Text
+              key={`${day}-${i}`}
+              style={{ width: DAY_SIZE }}
+              className="text-zinc-400 font-bold text-xl text-center"
+            >
+              {day}
+            </Text>
+          );
+        })}
+      </View>
+
+      <HabitDay />
     </View>
   );
 }
