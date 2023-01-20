0ed11f73c4b5e281dedd6468721a83c83f85683a
Rafael Tosta <rafa.ecomp@gmail.com>
2023-01-20 10:18:16 -0300

    componente: Header

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,7 +5,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#09090A"
+
+      }
+    },
   },
   plugins: [],
 }
