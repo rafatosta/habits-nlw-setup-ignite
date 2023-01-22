2a31e0deabb7130d810549e37347ef700b3672e3
Rafael Tosta <rafa.ecomp@gmail.com>
2023-01-22 17:01:56 -0300

    :computer: aula 5

--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -12,7 +12,7 @@ export function ProgressBar(props: ProgressBarProps) {
             value={props.progress}
         >
             <Progress.Indicator
-                className='h-3 rounded-xl bg-violet-600'
+                className='h-3 rounded-xl bg-violet-600 transition-all'
                 style={{
                     width: `${props.progress}%`
                 }} />
