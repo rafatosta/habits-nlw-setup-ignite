7106ff6a1d3f7783d42352c19730869f1bf0a205
Rafael Tosta <rafa.ecomp@gmail.com>
2023-01-22 15:15:38 -0300

    :iphone: aula 03 - React Native

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,37 +1,35 @@
 import './src/lib/dayjs';
-import { StatusBar } from "react-native";
-import { Home } from "./src/screens/Home";
 
-import {
+import { StatusBar } from 'react-native';
+import { 
   useFonts,
   Inter_400Regular,
   Inter_600SemiBold,
   Inter_700Bold,
-  Inter_800ExtraBold,
-} from "@expo-google-fonts/inter";
+  Inter_800ExtraBold
+} from '@expo-google-fonts/inter';
 
-import { Loading } from "./src/components/Loading";
+import { Loading } from './src/components/Loading';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
     Inter_700Bold,
-    Inter_800ExtraBold,
+    Inter_800ExtraBold
   });
 
   if (!fontsLoaded) {
-    return <Loading />;
+    return (
+      <Loading />
+    );
   }
 
   return (
     <>
-      <Home />
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
+      <Routes />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
     </>
   );
 }
--- mobile/src/components/Loading.tsx
+++ mobile/src/components/Loading.tsx
@@ -1,14 +1,9 @@
-import React from "react";
 import { ActivityIndicator, View } from "react-native";
 
 export function Loading() {
   return (
-    <View style={{ 
-        flex: 1, 
-        justifyContent: "center", 
-        alignItems: "center",
-        backgroundColor:'#09090A' }}>
-      <ActivityIndicator color="#7C3AED" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#09090A' }}>
+      <ActivityIndicator color="#7C3AED"/>
     </View>
-  );
+  )
 }
